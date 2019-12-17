09c54e911a4d848e36507367abd9a95c6d36ed15
jacektessen <jacek.tessen@gmail.com>
2019-12-17 19:47:55 +0100

    Admin panel updated

--- tasks/admin.py
+++ tasks/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from tasks import models
 
 
-admin.site.register(models.Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'content', 'column', 'order_no',
+                    'created_at', 'completed', 'user', 'user_id')
+    list_display_links = ('id', 'name', "user", "user_id")
+    list_filter = ('name', 'name', "created_at", "user", "user_id")
+    list_editable = ('completed',)
+    search_fields = ('name', 'user', 'user.id')
+    list_per_page = 50
+
+
+admin.site.register(models.Tasks, TasksAdmin)
 admin.site.register(models.Columns, models.columnsAdmin)
--- user/admin.py
+++ user/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 from user import models
 
 
-admin.site.register(models.UserProfile)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'name', 'is_active')
+    list_display_links = ('id', 'email', "name")
+    list_filter = ('email', "name")
+    list_editable = ('is_active',)
+    search_fields = ('email', 'name')
+    list_per_page = 50
+
+
+admin.site.register(models.UserProfile, UsersAdmin)
