c902b061b4af4e42bb99f76b9d96797d0416570b
jacektessen <jacek.tessen@gmail.com>
2019-12-21 20:20:34 +0100

    Updated SettingsPage model - default value

--- settings_page/models.py
+++ settings_page/models.py
@@ -12,7 +12,7 @@ class SettingsPage(models.Model):
     panel_1 = models.CharField(max_length=255, default="#ffcc80")
     panel_2 = models.CharField(max_length=255, default="#fff176")
     panel_3 = models.CharField(max_length=255, default="#ffeef2")
-    panel_4 = models.CharField(max_length=255, default="lightGreen")
+    panel_4 = models.CharField(max_length=255, default="#90ee90")
     language = models.CharField(
         max_length=255, choices=LANGUAGES, default="EN")
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, default=1)
