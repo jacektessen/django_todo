91a8d6255845f41c2761d977146f0fe4a4c72443
jacektessen <jacek.tessen@gmail.com>
2019-12-22 16:41:19 +0100

    SettingsPage model has added name of panels

--- settings_page/models.py
+++ settings_page/models.py
@@ -9,10 +9,14 @@ LANGUAGES = (
 
 
 class SettingsPage(models.Model):
-    panel_1 = models.CharField(max_length=255, default="#ffcc80")
-    panel_2 = models.CharField(max_length=255, default="#fff176")
-    panel_3 = models.CharField(max_length=255, default="#ffeef2")
-    panel_4 = models.CharField(max_length=255, default="#90ee90")
+    panel_1_color = models.CharField(max_length=255, default="#ffcc80")
+    panel_2_color = models.CharField(max_length=255, default="#fff176")
+    panel_3_color = models.CharField(max_length=255, default="#ffeef2")
+    panel_4_color = models.CharField(max_length=255, default="#90ee90")
+    panel_1_name = models.CharField(max_length=255, default="Important")
+    panel_2_name = models.CharField(max_length=255, default="Normal")
+    panel_3_name = models.CharField(max_length=255, default="Later")
+    panel_4_name = models.CharField(max_length=255, default="Done")
     language = models.CharField(
         max_length=255, choices=LANGUAGES, default="EN")
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, default=1)
